a097607b178dd37113673b5559a6b92563e597a2
Nick P <hubitor@users.noreply.github.com>
2019-11-16 13:35:06 +0100

    Add rest

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
-// Copyright IBM Corp. 2015,2019. All Rights Reserved.
-// Node module: loopback-sandbox
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-workspace
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
 
 'use strict';
 
